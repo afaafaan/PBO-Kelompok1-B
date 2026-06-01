f41b48acdbfdfd8d8a13fd9334c9215614919be3
muzzz <muznimubarok385@gmail.com>
2026-06-01 10:17:53 +0700

    Add Bebek class with makan, terbang, and berenang methods

--- implementasi_hewan.py
+++ implementasi_hewan.py
@@ -14,3 +14,13 @@ class Singa(Hewan):
 class Sapi(Hewan):
     def makan(self):
         print(f"{self.nama} sedang makan.")
+
+class Bebek(Hewan, BisaTerbang, BisaBerenang):
+    def makan(self):
+        print(f"{self.nama} sedang makan.")
+
+    def terbang(self):
+        print(f"{self.nama} sedang terbang.")
+
+    def berenang(self):
+        print(f"{self.nama} sedang berenang.")
